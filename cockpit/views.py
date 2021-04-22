d861f6c60273d0ba47f2d67078c3672147e32baf
rauanisanfelice <rauan.sanfelice@gmail.com>
2021-04-22 18:38:35 -0300

    🎨 Incluindo Rotas e Views

--- cockpit/views.py
+++ cockpit/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from django.contrib.auth.models import User
+
+from rest_framework import mixins, generics, permissions, viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, OperandHolder
+from rest_framework.schemas.openapi import AutoSchema
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+from .models import Ativos, Wallet, Transacao
+from .serializers import AtivosSerializer, WalletSerializer, TransacaoSerializer, UserSerializer
+
+logger = logging.getLogger(__name__)
+
+
+
+class CustomAuthToken(ObtainAuthToken):
+
+    schema = AutoSchema(tags=["Token"])
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
+
+class AtivoList(mixins.ListModelMixin, generics.GenericAPIView):
+    """Lista todos ativos."""
+
+    schema = AutoSchema(tags=["Ativos"])
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = Ativos.objects.all()
+    serializer_class = AtivosSerializer
+
+    def dispatch(self, request, *args, **kwargs):
+        logger.info(f"AtivoList {request.method} ({request.user.username})")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
--- core/urls.py
+++ core/urls.py
@@ -1,23 +1,23 @@
+from django.conf.urls import url
 from django.urls import path, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 from rest_framework.schemas import get_schema_view
 
-from api.views import UserList
 
 
 urlpatterns = [
     path('21232f297a57a5a743894a0e4a801fc3/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-
+    path('api/', include('cockpit.urls')),    
+    path('api/api-session-auth', include('rest_framework.urls', namespace='rest_framework')),
+    
     path('openapi-schema/', get_schema_view(
         title="Cockpit", 
         description="Django API Cockpit",
         version="1.0.0",
         public=True,
-    ), name='openapi-schema'), 
+    ), name='openapi-schema'),
 
     path('', TemplateView.as_view(
         template_name='swagger-ui.html',
